c5ff920f5bceeabf72fa932ec36a1b5e0f55c5a8
salmansk555 <salmanskk158@gmail.com>
2024-12-28 18:03:45 +0530

    first commit

---
+++ src/dec28/Employee.java
@@ -0,0 +1,30 @@
+package dec28 ;
+
+
+public class Employee {
+
+
+	
+	int id;
+	String name;
+	int salary;
+	String dept;
+	String address;
+	public Employee(int id, String name, int salary, String dept, String address) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.salary = salary;
+		this.dept = dept;
+		this.address = address;
+	}
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", salary=" + salary + ", dept=" + dept + ", address="
+				+ address + "]";
+	}
+	
+}
+
+
+	
